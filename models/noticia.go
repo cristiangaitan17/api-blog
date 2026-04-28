65d711a778c7e2cff38417749a3e98a271a1f136
cristiangaitan17 <cristiangaitan1717@icloud.com>
2026-04-28 17:13:02 -0500

    actualizacion completadaaa

--- models/noticia.go
+++ models/noticia.go
@@ -4,16 +4,16 @@ import "database/sql"
 
 type Noticia struct {
 	ID              int            `json:"id"`
-	CategoriaID     int            `json:"categoria_id"`
+	CategoriaID     sql.NullInt64  `json:"categoria_id"`
 	Titulo          string         `json:"titulo"`
 	Contenido       string         `json:"contenido"`
 	Encabezado      sql.NullString `json:"encabezado"`
 	ImagenPrincipal sql.NullString `json:"imagen_principal"`
-	AutorID         int            `json:"autor_id"`
+	AutorID         sql.NullInt64  `json:"autor_id"`
 	Estado          string         `json:"estado"`
 	Vistas          int            `json:"vistas"`
 	PublicadoEn     sql.NullString `json:"publicado_en"`
-	CreadoEn        string         `json:"creado_en"`
-	ActualizadoEn   string         `json:"actualizado_en"`
+	CreadoEn        sql.NullString `json:"creado_en"`      // Puede ser NULL
+	ActualizadoEn   sql.NullString `json:"actualizado_en"` // Puede ser NULL
 	Activo          bool           `json:"activo"`
 }
