0bbf1693418ceb573c0ad309b1368b785dd6d2c3
cristiangaitan17 <cristiangaitan1717@icloud.com>
2026-04-28 17:07:48 -0500

    se corrigieron errores

--- models/noticia.go
+++ models/noticia.go
@@ -1,17 +1,19 @@
 package models
 
+import "database/sql"
+
 type Noticia struct {
-	ID              int    `json:"id"`
-	CategoriaID     int    `json:"categoria_id"`
-	Titulo          string `json:"titulo"`
-	Contenido       string `json:"contenido"`
-	Encabezado      string `json:"encabezado"`
-	ImagenPrincipal string `json:"imagen_principal"`
-	AutorID         int    `json:"autor_id"`
-	Estado          string `json:"estado"`
-	Vistas          int    `json:"vistas"`
-	PublicadoEn     string `json:"publicado_en"`
-	CreadoEn        string `json:"creado_en"`
-	ActualizadoEn   string `json:"actualizado_en"`
-	Activo          bool   `json:"activo"`
+	ID              int            `json:"id"`
+	CategoriaID     int            `json:"categoria_id"`
+	Titulo          string         `json:"titulo"`
+	Contenido       string         `json:"contenido"`
+	Encabezado      sql.NullString `json:"encabezado"`
+	ImagenPrincipal sql.NullString `json:"imagen_principal"`
+	AutorID         int            `json:"autor_id"`
+	Estado          string         `json:"estado"`
+	Vistas          int            `json:"vistas"`
+	PublicadoEn     sql.NullString `json:"publicado_en"`
+	CreadoEn        string         `json:"creado_en"`
+	ActualizadoEn   string         `json:"actualizado_en"`
+	Activo          bool           `json:"activo"`
 }
