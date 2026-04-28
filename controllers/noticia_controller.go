e18c72a156c5741ae6ebb25a8aff81b9f224e19c
cristiangaitan17 <cristiangaitan1717@icloud.com>
2026-04-28 16:00:18 -0500

    creamos un nuevo archivo y agregamos las importaciones

---
+++ controllers/noticia_controller.go
@@ -0,0 +1,11 @@
+package controllers
+
+import (
+	"database/sql"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/cristiangaitan17/api-blog/config"
+	"github.com/cristiangaitan17/api-blog/models"
+)
