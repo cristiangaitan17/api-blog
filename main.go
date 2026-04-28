b90a22fe3869113e17221614921b3bc481300e96
cristiangaitan17 <cristiangaitan1717@icloud.com>
2026-04-28 16:06:38 -0500

    se agrego noticias en main

--- main.go
+++ main.go
@@ -12,7 +12,10 @@ func main() {
 	config.InitDB()
 
 	router := gin.Default()
+
+	// Registrar rutas
 	routes.CategoriaRoutes(router)
+	routes.NoticiaRoutes(router)
 
 	log.Println("🚀 Servidor corriendo en http://localhost:8080")
 	log.Fatal(router.Run(":8080"))
