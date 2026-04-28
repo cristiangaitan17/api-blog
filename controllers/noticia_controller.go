433e2d78f00cb4d8052c4cab5b343674e761b0d5
cristiangaitan17 <cristiangaitan1717@icloud.com>
2026-04-28 16:01:25 -0500

    funcion para obtener todas las noticias

--- controllers/noticia_controller.go
+++ controllers/noticia_controller.go
@@ -9,3 +9,33 @@ import (
 	"github.com/cristiangaitan17/api-blog/config"
 	"github.com/cristiangaitan17/api-blog/models"
 )
+
+func GetNoticias(c *gin.Context) {
+	rows, err := config.DB.Query(`
+		SELECT id, categoria_id, titulo, contenido, encabezado, 
+		       imagen_principal, autor_id, estado, vistas, publicado_en,
+		       creado_en, actualizado_en, activo
+		FROM blog.noticias
+	`)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer rows.Close()
+
+	var noticias []models.Noticia
+	for rows.Next() {
+		var n models.Noticia
+		err := rows.Scan(
+			&n.ID, &n.CategoriaID, &n.Titulo, &n.Contenido, &n.Encabezado,
+			&n.ImagenPrincipal, &n.AutorID, &n.Estado, &n.Vistas, &n.PublicadoEn,
+			&n.CreadoEn, &n.ActualizadoEn, &n.Activo,
+		)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		noticias = append(noticias, n)
+	}
+	c.JSON(http.StatusOK, noticias)
+}
