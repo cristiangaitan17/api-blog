3b65f1c9c2aa0542ba52a5ca0387c538070b0001
cristiangaitan17 <cristiangaitan1717@icloud.com>
2026-04-28 17:20:52 -0500

    actualizacionn

--- controllers/noticia_controller.go
+++ controllers/noticia_controller.go
@@ -4,8 +4,7 @@ import (
 	"database/sql"
 	"net/http"
 	"strconv"
-	"time"
-
+	
 	"github.com/gin-gonic/gin"
 	"github.com/cristiangaitan17/api-blog/config"
 	"github.com/cristiangaitan17/api-blog/models"
