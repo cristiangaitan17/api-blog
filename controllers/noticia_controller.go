31852f467a6e1999ffef749dea8b4ea5a16450e7
cristiangaitan17 <cristiangaitan1717@icloud.com>
2026-04-28 16:04:12 -0500

    funcion para agregar una noticia nueva

--- controllers/noticia_controller.go
+++ controllers/noticia_controller.go
@@ -70,3 +70,27 @@ func GetNoticiaByID(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, n)
 }
+
+func CreateNoticia(c *gin.Context) {
+	var n models.Noticia
+	if err := c.ShouldBindJSON(&n); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	query := `
+		INSERT INTO blog.noticias (categoria_id, titulo, contenido, encabezado, 
+		       imagen_principal, autor_id, estado, vistas, publicado_en,
+		       creado_en, actualizado_en, activo)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, NOW(), NOW(), $10)
+		RETURNING id, creado_en, actualizado_en
+	`
+	err := config.DB.QueryRow(query, n.CategoriaID, n.Titulo, n.Contenido, n.Encabezado,
+		n.ImagenPrincipal, n.AutorID, n.Estado, n.Vistas, n.PublicadoEn, n.Activo).
+		Scan(&n.ID, &n.CreadoEn, &n.ActualizadoEn)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, n)
+}
